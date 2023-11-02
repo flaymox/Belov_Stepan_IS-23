7aeca4480aa8f92c6f6da6e060b5bc248130de8e
flaymox <yanavedenova@gmail.com>
2023-11-02 18:29:29 +0300

    enter pz3.2

--- PZ3/PZ_3_2.py
+++ PZ3/PZ_3_2.py
@@ -1,15 +1,13 @@
-def calculate_discount(purchase_amount):
-    if purchase_amount < 500:
-        discount = "2%"
-    elif 500 <= purchase_amount < 1000:
-        discount = "3%"
-    elif 1000 <= purchase_amount < 1500:
-        discount = "4%"
-    elif 1500 <= purchase_amount < 2000:
-        discount = "5%"
-    else:
-        discount = "0%"
-    return discount
 purchase_amount = float(input("Введите сумму покупки в рублях: "))
-discount = calculate_discount(purchase_amount)
-print(f"Размер скидки: {discount}")
+if purchase_amount < 500:
+        discount = 2
+elif 500 <= purchase_amount < 1000:
+        discount = 3
+elif 1000 <= purchase_amount < 1500:
+        discount = 4
+elif 1500 <= purchase_amount < 2000:
+        discount = 5
+else:
+        discount = 0
+
+print(f"Размер скидки: {discount}%")
