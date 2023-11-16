5eb9e6cca7e57de880fe37d745f4744d8bfa6cac
flaymox <yanavedenova@gmail.com>
2023-11-16 19:11:44 +0300

    correction pz3

--- PZ3/PZ_3_2.py
+++ PZ3/PZ_3_2.py
@@ -1,12 +1,13 @@
+# Разработать программу, выводящую на экран размер скидки на продукты.
 purchase_amount = float(input("Введите сумму покупки в рублях: "))
-if purchase_amount > 0:
-        if purchase_amount < 500:
+if purchase_amount > 0: # Обработка исключений
+        if purchase_amount < 500: # Обработка исключений
                 discount = 2
-        elif 500 <= purchase_amount < 1000:
+        elif 500 <= purchase_amount < 1000: # Обработка исключений
                 discount = 3
-        elif 1000 <= purchase_amount < 1500:
+        elif 1000 <= purchase_amount < 1500: # Обработка исключений
                 discount = 4
-        elif 1500 <= purchase_amount < 2000:
+        elif 1500 <= purchase_amount < 2000: # Обработка исключений
                 discount = 5
         else:
                 discount = 0
