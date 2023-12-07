035fc96e1c675239ab8b34cb0306b3dcb64398e6
flaymox <yanavedenova@gmail.com>
2023-12-07 19:32:37 +0300

    add pz5.1

---
+++ PZ5/PZ_5_1.py
@@ -0,0 +1,9 @@
+def sum_of_series(n):
+    result = 0
+    for i in range(1, n + 1):
+        result += i
+    return result
+
+n = int(input("Введите число:"))
+result_sum = sum_of_series(n)
+print(f"Сумма числового ряда до {n} равна {result_sum}")
