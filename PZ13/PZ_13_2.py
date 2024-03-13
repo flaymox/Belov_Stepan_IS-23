58602e1ca84ad4f0174d6de77f4d09031a6d2ec5
flaymox <yanavedenova@gmail.com>
2024-03-13 10:00:27 +0300

    pz12

---
+++ PZ13/PZ_13_2.py
@@ -0,0 +1,19 @@
+# Исходная матрица
+matrix = [
+    [1, 2, 3],
+    [4, -5, 6],
+    [-7, 8, 9]
+]
+
+# Формируем массив из положительных четных элементов
+positive_even_numbers = [num for row in matrix for num in row if num > 0 and num % 2 == 0]
+
+# Вычисляем сумму и среднее арифметическое
+sum_of_numbers = sum(positive_even_numbers)
+average_of_numbers = sum_of_numbers / len(positive_even_numbers) if positive_even_numbers else 0
+
+# Выводим результаты
+print("Массив из положительных четных элементов:", positive_even_numbers)
+print("Сумма:", sum_of_numbers)
+print("Среднее арифметическое:", average_of_numbers)
+
