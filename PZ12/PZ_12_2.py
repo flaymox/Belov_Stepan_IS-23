0c38c237a44e5c37a994f275b879514f0fe87310
flaymox <yanavedenova@gmail.com>
2024-03-13 10:37:16 +0300

    pz12 AND pz13

--- PZ12/PZ_12_2.py
+++ PZ12/PZ_12_2.py
@@ -1,3 +1,5 @@
+# Составить генератор (yield), который выводит из строки только цифры.
+
 string = input("Введите строку: ")
 digits_generator = (int(char) for char in string if char.isdigit())
 print(list(digits_generator))
